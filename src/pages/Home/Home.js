93cac135e0882f81bdc466a3f0c0b21b0d5ebfac
CosmoBarbosa <159724797+CosmoBarbosa@users.noreply.github.com>
2025-05-07 20:30:25 -0300

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,18 +8,21 @@ export default function Home() {
 		{
 			nome: "maçã",
 			cor: "vermelha",
+			peso: 1000,
 			image:
 				"https://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg",
 		},
 		{
 			nome: "banana",
 			cor: "amarela",
+			peso: 1000,
 			image:
 				"https://upload.wikimedia.org/wikipedia/commons/a/af/Bananas_%28Alabama_Extension%29.jpg",
 		},
 		{
 			nome: "uva",
 			cor: "roxa",
+			peso: 1000,
 			image:
 				"https://upload.wikimedia.org/wikipedia/commons/d/df/ConcordGrapes.jpg",
 		},
@@ -27,14 +30,49 @@ export default function Home() {
 	function Reder({ item }) {
 		console.log(item);
 		return (
-			<View style={{flexDirection:"row", alignItems:"center", backgroundColor:"cyan", margin:5}} >
-				<Image
-					style={styles.logo}
-					source={{	uri: item.image,}}
-				/>
-				<Text>
-					Nome da fruta: {item.nome} cor da fruta:{item.cor}
-				</Text>
+			<View style={{
+				flexDirection: "row",
+				alignSelf: "center",
+				margin: 5,
+				alignItems: "center",
+				height: 68,
+				width: "95%",
+				shadowColor: "#000",
+				shadowRadius: 5.84,
+				elevation: 5,
+				borderRadius:10,
+				backgroundColor:"#fff"
+				
+			}}>
+				<View
+					style={{
+						flexDirection: "row",
+						borderRadius:10,
+						alignSelf: "center",
+						alignItems: "center",
+						height: 70,
+						width: "100%",
+						backgroundColor:"#fff"
+					}}
+				>
+					<View
+						style={{
+							borderRadius: 80,
+							margin:10,
+							justifyContent: "center",
+							alignItems: "center",
+							backgroundColor:"#fff"
+							
+						}}
+					>
+						<Image style={styles.logo} source={{ uri: item.image }} />
+					</View>
+					<View style={{borderRadius: 8,}}>
+						<Text>Nome: {item.nome}</Text>
+						<Text>Cor: {item.cor}</Text>
+						<Text>Peso (g): {item.peso}</Text>
+					</View>
+				</View>
 			</View>
 		);
 	}
@@ -50,17 +88,15 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		justifyContent: "center",
-		alignItems: "center",
 	},
 	lista: {
-		alignSelf: "center",
 		marginTop: 140,
+		width: "100%",
 	},
 	logo: {
 		width: 50,
 		height: 50,
-		marginBottom: 40,
 		borderRadius: 100,
-		backgroundColor: "red",
+		alignSelf:"center"
 	},
 });
